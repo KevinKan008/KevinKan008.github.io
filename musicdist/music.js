c7606c4d530b3125334337d3597d2cbc7176c410
KevinKan008 <qfkuang@126.com>
2020-02-22 03:39:27 +0800

    Site updated: 2020-02-22 03:39:17

--- musicdist/music.js
+++ musicdist/music.js
@@ -2,24 +2,28 @@ const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
     autoplay: false,
+    lrcType: 3,
     audio: [
       {
         name: "晴天",
         artist: "周杰伦",
         url: "/medias/music/qingtian.mp3",
         cover: "/medias/music/yehuimei.jpg",
+        lrc: "/medias/music/qingt.lrc",
       },
       {
         name: "夜的第七章",
         artist: "周杰伦",
         url: "/medias/music/yededi.mp3",
         cover: "/medias/music/yede.jpg",
+        lrc: "/medias/music/yedeqi.lrc",
       },
       {
         name: "火车叨位去",
         artist: "周杰伦",
         url: "/medias/music/huoche.mp3",
         cover: "/medias/music/badu.jpg",
+        lrc: "/medias/music/huoc.lrc",
       },
     ]
 });
